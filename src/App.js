f7ec1cc622d3316ac10f8a99f8e768873479706c
Josh Mendoza <jacinto.josh.mendoza@gmail.com>
2024-02-20 15:06:45 -0600

    Created buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import Button from "./Button";
 
 function App() {
-    return <div>
-        <div>
-            <Button succes rounded outline>Hello</Button>
-        </div>
-        <div>
-            <Button>Button 2</Button>
-        </div>
-        <div>
-            <Button>3 button</Button>
-        </div>
-        <div>
-            <Button>4 btn</Button>
-        </div>
+  return <div>
+    <div>
+      
+      <Button secondary rounded outline>Hello</Button>
     </div>
+    <div>
+      <Button danger outline>Button 2</Button>
+    </div>
+    <div>
+      <Button warning>3 button</Button>
+    </div>
+    <div>
+      <Button secondary outline>4 btn</Button>
+    </div>
+    <div>
+      <Button primary rounded>Something! </Button>
+    </div>
+  </div>
 }
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types' // Prop types aren't as popular as TS
+import classNames from 'classnames';
 
 function Button({
     children,
@@ -10,24 +10,39 @@ function Button({
     outline,
     rounded,
 }) {
-    return <button className='bg-red-500'>{children}</button>;
+    const classes = classNames('px-3 py-1.5 border', {
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-green-500 bg-green-500 text-white': success,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger,
+    });
+
+    return <button className={classes}>{children}</button>;
 }
 
 Button.propTypes = {
     checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
-        const count = Number(!!primary)
-            + Number(!!secondary)
-            + Number(!!success)
-            + Number(!!warning)
-            + Number(!!danger)
+        const count =
+            Number(!!primary) +
+            Number(!!secondary) +
+            Number(!!warning) +
+            Number(!!success) +
+            Number(!!danger);
 
         if (count > 1) {
             return new Error(
-                'Only one btn can be true'
+                'Only one of primary, secondary, success, warning, danger can be true'
             );
         }
-    }
-
-}
+    },
+};
 
 export default Button;
