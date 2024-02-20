2dd40ac4f7398c63f48727e6d54b5908a222abdb
Josh Mendoza <jacinto.josh.mendoza@gmail.com>
2024-02-20 14:43:12 -0600

    tailwind install and test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div>
+        <div>
+            <Button succes rounded outline>Hello</Button>
+        </div>
+        <div>
+            <Button>Button 2</Button>
+        </div>
+        <div>
+            <Button>3 button</Button>
+        </div>
+        <div>
+            <Button>4 btn</Button>
+        </div>
     </div>
-  );
 }
 
 export default App;
